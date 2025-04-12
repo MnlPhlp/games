c5e88f17a263bb68faadbcabee0133b3e743a150
MnlPhlp <online@philipp-manuel.de>
2025-04-12 02:13:41 +0200

    update .gitignore and build script;

--- buildAll.sh
+++ buildAll.sh
@@ -9,16 +9,18 @@ metadata=$(cargo metadata --format-version=1 --no-deps)
 member_paths=$(echo "$metadata" | jq -r '.packages[] | select(.source == null) | .manifest_path' | xargs -I {} dirname {})
 game_list=""
 
+rm -rf _site
 mkdir -p _site
+
 # Loop through each member path and build it
 for path in $member_paths; do
     echo "Building $path"
     game=$(basename "$path")
-    game_list="$game_list\n    <li>$game</li>"
+    game_list="$game_list\n    <li><a href="/$game"/>$game</li>"
     # Change to the member directory
     cd "$path"
     # Build the member
-    trunk build --release  --public-url "/$game"
+    trunk build --public-url "/$game"
     # Change back to the original directory
     cd -
     # Copy the built files to the _site directory
@@ -29,4 +31,4 @@ done
 
 cp public/index.html _site
 sed -i "s|GAME_LIST|$game_list|" _site/index.html
-cp public/style.css _site
+cp public/styles.css _site
